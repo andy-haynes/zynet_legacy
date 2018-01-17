dfd66ad6d92cefaa5bbf039b095a77f04dec35df
Andy Haynes <andyghaynes@gmail.com>
2018-01-16 22:43:02 -0800

    controller: don't switch on PID logic until temperature is x% of target temperature prevents PID from skewing over long heating times

--- controller/config.ts
+++ controller/config.ts
@@ -2,8 +2,11 @@ import { w3cwebsocket } from 'websocket';
 
 
 export default {
+  PID: {
+    strikeThreshold: 0.95 // percentage of target temp to reach before switching to PID control
+  },
   thermometer: {
-    sampleIntervalMS: 750
+    sampleIntervalMS: 750 // ms between temperature measurements
   },
   websocket: {
     url: 'ws://localhost:8081',
--- controller/controller.ts
+++ controller/controller.ts
@@ -1,4 +1,5 @@
 import BrewUpdate from '../src/models/BrewUpdate';
+import config from './config';
 import { MockRelay, MockThermometer } from './mocks';
 import { PIDController } from './pid';
 import ZynetConnection from './connection';
@@ -9,15 +10,23 @@ const mockRelay = new MockRelay();
 const mockThermometer = new MockThermometer(mockRelay);
 const mockUpdate = new BrewUpdate(1, 1, 60, 68, 152, false, 0);
 
-const pidController = new PIDController(1.5, 1, 0.5, 3);
-pidController.setTargetTemperature(75);
-
 const connection = new ZynetConnection();
 connection.connect();
 
+let pidThresholdReached = false;
+let targetTemperature = 80;
+
+const pidController = new PIDController(1.5, 1, 0.5, 3);
+pidController.setTargetTemperature(targetTemperature);
+
 mockThermometer.sensor$.subscribe((temperature: number) => {
-  pidController.updateTemperature(temperature);
-  mockRelay.switch(pidController.state);
+  if (pidThresholdReached) {
+    pidController.updateTemperature(temperature);
+    mockRelay.switch(pidController.state);
+  } else {
+    pidThresholdReached = temperature >= (targetTemperature * config.PID.strikeThreshold);
+    mockRelay.switch(!pidThresholdReached);
+  }
 
   connection.send(new ZynetMessage(
     ZynetMessageType.LogUpdate,
