0f0a885b3cde2af80187c9930e63f94f8ff8f82a
Andy Haynes <andyghaynes@gmail.com>
2018-02-03 23:24:55 -0800

    deployment script/config with scp

---
+++ src/deployment.config.ts
@@ -0,0 +1,36 @@
+const remote = {
+  password: 'autobot',
+  path: '/home/dinobot/zynet/',
+  user: 'dinobot',
+};
+const copyPaths = [
+  'src/config.ts',
+  'src/models/',
+  'src/constants/',
+  'package.json',
+];
+
+function remoteConfig(host: string, paths: string[]) {
+  return {
+    host,
+    copyPaths: paths.concat(copyPaths),
+    password: remote.password,
+    path: remote.path,
+    user: remote.user,
+  };
+}
+
+export default {
+  controller: remoteConfig('swoop', ['controller/']),
+  server: remoteConfig('grimlock', ['server/']),
+  platform: {
+    osx: {
+      projectPath: '/Users/andy/projects/zynet',
+      serverSuffix: '.local',
+    },
+    windows: {
+      pathTransform: (path) => path.replace(/\//g, '\\'),
+      projectPath: 'C:\\projects\\zynet',
+    }
+  }
+};
